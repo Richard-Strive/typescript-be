785dbf45263683180a5447d9c5472e32d2bb7079
Richard Johnson Addai <72205808+Richard-Strive@users.noreply.github.com>
2021-06-21 12:50:40 +0200

    middleware shifted to tools.ts

---
+++ src/routes/tools.ts
@@ -0,0 +1,38 @@
+import { check } from "express-validator";
+
+const valideMid = () => {
+  return [
+    check("firstName", "Please the field shouldn't be empty")
+      .not()
+      .isEmpty()
+      .isAlpha(),
+    check("surname", "Please the field shouldn't be empty")
+      .not()
+      .isEmpty()
+      .isAlpha(),
+    check(
+      "title",
+      "Please the field shouldn't be empty or it should contain letters"
+    )
+      .not()
+      .isEmpty()
+      .isAlpha(),
+    check("email", "Please choose a valid email").isEmail(),
+    check(
+      "phoneNumber",
+      "Please choose a IT format phone number"
+    ).isMobilePhone(["it-IT"]),
+    check("locationOfRecidence", "This field shouldn't be Empty")
+      .not()
+      .isEmpty()
+      .isLength({
+        min: 5,
+      }),
+    check(
+      "password",
+      "Please make sure to add special chars and numbers to your password"
+    ).isStrongPassword(),
+  ];
+};
+
+export default valideMid;
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,45 +1,12 @@
 import express, { Request, Response, NextFunction } from "express";
 
-import { body, validationResult, check } from "express-validator";
+import { body, validationResult } from "express-validator";
+
+import valideMid from "../tools";
 
 import User from "./schema";
 
 const route = express.Router();
-
-const valideMid = () => {
-  return [
-    check("firstName", "Please the field shouldn't be empty")
-      .not()
-      .isEmpty()
-      .isAlpha(),
-    check("surname", "Please the field shouldn't be empty")
-      .not()
-      .isEmpty()
-      .isAlpha(),
-    check(
-      "title",
-      "Please the field shouldn't be empty or it should contain letters"
-    )
-      .not()
-      .isEmpty()
-      .isAlpha(),
-    check("email", "Please choose a valid email").isEmail(),
-    check(
-      "phoneNumber",
-      "Please choose a IT format phone number"
-    ).isMobilePhone(["it-IT"]),
-    check("locationOfRecidence", "This field shouldn't be Empty")
-      .not()
-      .isEmpty()
-      .isLength({
-        min: 5,
-      }),
-    check(
-      "password",
-      "Please make sure to add special chars and numbers to your password"
-    ).isStrongPassword(),
-  ];
-};
 
 //Registration
 route.post(
